df573e5e26f9fb898b29ce057a02256c012b5e4e
Jono Prest <jjprest@gmail.com>
2023-10-19 15:08:00 +0200

    add dotenv and concurrently for gql codegen

--- packages/nextjs/codegen.ts
+++ packages/nextjs/codegen.ts
@@ -1,9 +1,12 @@
 import { CodegenConfig } from "@graphql-codegen/cli";
+import dotenv from "dotenv";
 
+//Get the gql url defined in environment
+dotenv.config({ path: "./.env.local" });
 export const localHasuraEndpoint = "http://localhost:8080/v1/graphql";
 
 const config: CodegenConfig = {
-  schema: localHasuraEndpoint,
+  schema: process.env.NEXT_PUBLIC_GRAPHQL_URL ?? localHasuraEndpoint,
   // this assumes that all your source files are in a top-level `src/` directory - you might need to adjust this to your file structure
   documents: ["pages/**/*.{ts,tsx}", "components/**/*.{ts,tsx}"],
   generates: {
